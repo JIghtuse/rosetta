883e28802289c14034bb8c46614cb3d41c5eefcd
Boris Egorov <jightuse@gmail.com>
2014-01-10 21:41:01 +0700

    Fix indenting of isatty

--- input_is_terminal/rust/isatty.rs
+++ input_is_terminal/rust/isatty.rs
@@ -2,10 +2,10 @@ extern mod std;
 use std::libc;
 
 fn main() {
-	let istty = unsafe { libc::isatty(libc::STDIN_FILENO as i32) } != 0;
-	if (istty) {
-		println("stdin is tty");
-	} else {
-		println("stdin is not tty");
-	}
+    let istty = unsafe { libc::isatty(libc::STDIN_FILENO as i32) } != 0;
+    if (istty) {
+        println("stdin is tty");
+    } else {
+        println("stdin is not tty");
+    }
 }
