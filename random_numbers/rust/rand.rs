11cc669020903e359ef2ff0237558e61e2bef2d7
Boris Egorov <jightuse@gmail.com>
2014-02-05 15:12:15 +0700

    Simplify array creation in random generation

--- random_numbers/rust/rand.rs
+++ random_numbers/rust/rand.rs
@@ -2,7 +2,7 @@ use std::rand;
 use std::rand::distributions::{Normal, IndependentSample};
 
 fn main() {
-    let mut rands: [f64, ..1000] = [0.0, ..1000];
+    let mut rands = [0.0, ..1000];
     let normal = Normal::new(1.0, 0.5);
 
     for i in range(0, 1000) {
